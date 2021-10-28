6b99a91593035b99b9656309a9593001540674bd
Peter Donovan <p.v.donovan4@gmail.com>
2021-10-27 18:35:40 -0700

    Add splitter implementations.

---
+++ textprobability/splitters.py
@@ -0,0 +1,10 @@
+"""This module implements common schemes for splitting strings into linguistic units."""
+
+from textprobability.types import Splitter
+import re
+
+
+latin_tokens: Splitter = lambda s: [
+    w for w in re.split(r"(?<=[^\w�])|(?=[^\w�])", s) if w.strip()
+]
+characters: Splitter = lambda s: s  # A string is an iterable of strings.
